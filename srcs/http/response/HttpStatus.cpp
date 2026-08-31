e96c82c6477daa1fd30c6806f5f0be3ea30c7134
Marta Grandia Gutierrez <mgrandia@cbr12s5.42barcelona.com>
2026-08-31 12:40:26 +0200

    feat: error pages created

--- srcs/http/response/HttpStatus.cpp
+++ srcs/http/response/HttpStatus.cpp
@@ -6,16 +6,35 @@
 /*   By: mgrandia <mgrandia@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/31 12:13:43 by mgrandia          #+#    #+#             */
-/*   Updated: 2026/08/31 11:32:06 by mgrandia         ###   ########.fr       */
+/*   Updated: 2026/08/31 12:39:19 by mgrandia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "http/HttpStatus.hpp"
+#include <sstream>
 
 HttpStatusInfo::HttpStatusInfo(const std::string& reason, const std::string& body)
 {
 	reasonPhrase = reason;
 	defaultBody = body;
+}
+
+std::string generateErrorPage(const std::string& message)
+{
+	std::stringstream ss;
+
+	ss << "<!DOCTYPE html>"
+	<< "<html>"
+	<< "<head>"
+	<< "<title>" << message << "</title>"
+	<< "<link rel=\"stylesheet\" href=\"/style.css\">"
+	<< "</head>"
+	<< "<body>"
+	<< "<h1>" << message << "</h1>"
+	<< "</body>"
+	<< "</html>";
+
+	return ss.str();
 }
 
 HttpStatusInfo getStatusInfo(int statusCode)
@@ -27,22 +46,22 @@ HttpStatusInfo getStatusInfo(int statusCode)
 		case 201:
 			return HttpStatusInfo("Created", "Created");
 		case 400:
-			return HttpStatusInfo("Bad Request", "400 Bad Request");//        "<html><body><h1>400 Bad Request</h1></body></html>"
+			return HttpStatusInfo("Bad Request", generateErrorPage("400 Bad Request"));
 		case 403:
-			return HttpStatusInfo("Forbidden", "403 Forbidden");
+			return HttpStatusInfo("Forbidden", generateErrorPage("403 Forbidden"));
 		case 404:
-			return HttpStatusInfo("Not Found", "404 Not Found");
+			return HttpStatusInfo("Not Found", generateErrorPage("404 Not Found"));
 		case 405:
-			return HttpStatusInfo("Method Not Allowed", "405 Method Not Allowed");
+			return HttpStatusInfo("Method Not Allowed", generateErrorPage("405 Method Not Allowed"));
 		case 408: // TODO add control errors on ServerManager, example one 408
-			return HttpStatusInfo("Request Time-out", "408 Request Time-out");
+			return HttpStatusInfo("Request Time-out", generateErrorPage("408 Request Time-out"));
 		case 413:
-			return HttpStatusInfo("Payload Too Large", "413 Payload Too Large");
+			return HttpStatusInfo("Payload Too Large", generateErrorPage("413 Payload Too Large"));
 		case 500:
-			return HttpStatusInfo("Internal Server Error", "500 Internal Server Error");
+			return HttpStatusInfo("Internal Server Error", generateErrorPage("500 Internal Server Error"));
 		case 501:
-			return HttpStatusInfo("Not Implemented", "501 Not Implemented");
+			return HttpStatusInfo("Not Implemented", generateErrorPage("501 Not Implemented"));
 		default:
-			return HttpStatusInfo("Internal Server Error", "500 Internal Server Error");
+			return HttpStatusInfo("Internal Server Error", generateErrorPage("500 Internal Server Error"));
     }
 }
